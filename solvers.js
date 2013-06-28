0a40c804111deed8e47f63ec7bfaa4ce0b61cf89
Hack Reactor Students <github@hackreactor.com>
2013-06-27 21:57:57 -0700

    working on the n queens solution

--- solvers.js
+++ solvers.js
@@ -2,15 +2,34 @@
 // hint: you'll need to do a full-search of all possible arrangements of pieces!
 // (There are also optimizations that will allow you to skip a lot of the dead search space)
 
+
+ var makeEmptyMatrix = function(n){
+    return _(_.range(n)).map(function(){
+      return _(_.range(n)).map(function(){
+        return 0;
+      });
+    });
+  };
+
+
 window.findNRooksSolution = function(n){
-  var solution = undefined; //fixme
+
+  var solution = undefined;
 
   console.log('Single solution for ' + n + ' rooks:', solution);
   return solution;
 };
 
 window.countNRooksSolutions = function(n){
-  var solutionCount = undefined; //fixme
+
+  var factorial = function (n){ 
+    if (n===0){
+      return 1;
+    } else {
+      return (n * factorial(n - 1));
+    }
+  };
+  var solutionCount = factorial(n);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
@@ -24,8 +43,24 @@ window.findNQueensSolution = function(n){
 };
 
 window.countNQueensSolutions = function(n){
-  var solutionCount = undefined; //fixme
-
+  //debugger;
+  var matrix= makeEmptyMatrix(n); 
+  var solutionCount = 0;
+  //_.each(matrix, function(rows){
+  var firstRow = matrix[0];
+    var walkMatrix = function(firstRow){
+        for( var i = 0; i < n; i++){
+          board.togglePiece(firstRow, i);//toggle on
+              if(this.hasAnyQueenConflictsOn(rowIndex,i)){
+                board.togglePiece(firstRow,i);//toggle off
+                walkMatrix(firstRow+1);
+              } else {
+                walkMatrix(firstRow +1);
+              }
+        }
+    };
+  //});
+  
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
