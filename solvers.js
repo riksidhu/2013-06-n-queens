e0f6be581433b2a4cafa032063d2f00aaf5db885
Hack Reactor Students <github@hackreactor.com>
2013-06-27 23:37:46 -0700

    work in progress

--- solvers.js
+++ solvers.js
@@ -3,14 +3,24 @@
 // (There are also optimizations that will allow you to skip a lot of the dead search space)
 
 
- var makeEmptyMatrix = function(n){
-    return _(_.range(n)).map(function(){
+   var makeEmptyMatrix = function(n){
       return _(_.range(n)).map(function(){
-        return 0;
+        return _(_.range(n)).map(function(){
+          return 0;
+        });
       });
-    });
-  };
+    };
 
+window.toggle = function (array) {
+  if (array[0] === 1) {
+    array[0] = 0;
+    return array[0];
+  }
+  if (array[0] === 0) {
+     array[0]  = 1;
+     return array[0];
+  }
+};
 
 window.findNRooksSolution = function(n){
 
@@ -43,29 +53,32 @@ window.findNQueensSolution = function(n){
 };
 
 window.countNQueensSolutions = function(n){
-  //debugger;
-  var matrix= makeEmptyMatrix(n); 
+  debugger;
+
+  var matrix = makeEmptyMatrix(n);
   var solutionCount = 0;
-  //_.each(matrix, function(rows){
-  var firstRow = matrix[0];
-    var walkMatrix = function(firstRow){
-        for(var i = 0; i < n; i++){
-          board.togglePiece(firstRow, i);//toggle on
-              if(!this.hasAnyQueenConflictsOn(firstRow,i)){
-                if(firstRow +1 < n){
-                  walkMatrix(firstRow+1);
-                } else{
-                  solutionCount++;
-                }
-              //   board.togglePiece(firstRow,i);//toggle off
-              // } else {
-              //   walkMatrix(firstRow +1);
-          }
-          board.toggle(firstRow,i);
-        }
-    };
-  //});
-  
+    for(var i = 0; i < n; i++){
+
+
+    }
+
+    // var walkMatrix = function(firstRow){
+    //     for(var i = 0; i < n; i++){
+    //       board.togglePiece(firstRow, i);//toggle on
+    //           if(!this.hasAnyQueenConflictsOn(firstRow,i)){
+    //             if(firstRow +1 < n){
+    //               walkMatrix(firstRow+1);
+    //             } else{
+    //               solutionCount++;
+    //             }
+    //           //   board.togglePiece(firstRow,i);//toggle off
+    //           // } else {
+    //           //   walkMatrix(firstRow +1);
+    //       }
+    //       board.toggle(firstRow,i);
+    //     }
+    // };
+
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
